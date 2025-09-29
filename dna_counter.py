ec121d27c9df25345a9df1c59f9c4ccfc52ef913
Arnav <91401687+ArnavDevelops@users.noreply.github.com>
2025-09-30 02:40:46 +0530

    GUI upgrade
    Moved from tkinter to custom tkinter.

--- dna_counter.py
+++ dna_counter.py
@@ -1,26 +1,50 @@
-import tkinter as tk
-from tkinter import messagebox, ttk
+from PIL import Image
 import random
+import customtkinter as ctk
 
 def random_dna(length):
     return ''.join(random.choice("ATGC") for _ in range(length))
 
-root = tk.Tk()
+ctk.set_appearance_mode("light")
+ctk.set_default_color_theme("blue")
+root = ctk.CTk()
 root.title("DNA Counter")
 root.geometry("450x400")
 
-tk.Label(root, text="Input DNA Sequence:", font=("Calligraffitti", 10)).pack(pady=5)
-entry = tk.Entry(root, width=40, borderwidth=2, font=("Calligraffitti", 10))
+ctk.CTkLabel(root, text="Input DNA Sequence:", font=("Calligraffitti", 10))
+entry = ctk.CTkEntry(root, width=250, border_width=2, font=("Calligraffitti", 12), corner_radius=5)
 entry.pack(pady=5)
 
-result_label = tk.Label(root, text="", justify="left", font=("Calligraffitti", 10))
+randomize_img = Image.open("images/3580329.png")
+
+def fill_random():
+    seq = random_dna(30) 
+    entry.delete(0, ctk.END)
+    entry.insert(0, seq)
+ctk.CTkButton(root, text="Randomize", 
+              command=fill_random, 
+              text_color="white", 
+              font=("Calligraffitti", 14), 
+              corner_radius=5,
+              hover_color="#6969E1",
+              image=ctk.CTkImage(dark_image=randomize_img, light_image=randomize_img)).pack(pady=10)
+
+line = ctk.CTkFrame(root, height=2, width=400, fg_color="gray")
+line.pack(pady=10)
+
+result_label = ctk.CTkLabel(root, text="", font=("Calligraffitti", 14))
 result_label.pack(pady=10)
 
 def count_dna():  
     dna = entry.get().upper()
     
     if len(dna) == 0 or any(base not in "ATGC" for base in dna):
-        messagebox.showerror("Error", "Enter only A, T, G, C!")
+        error_window = ctk.CTkToplevel(root)
+        error_window.title("Error!")
+        error_window.geometry("200x120")
+        error_window.wm_transient(root)
+        error_window.resizable(False, False)
+        ctk.CTkLabel(error_window, text="Invalid Sequence!", font=("Calligraffitti", 12)).pack(pady=10)
         return
 
     a_count = dna.count('A')
@@ -31,8 +55,7 @@ def count_dna():
     GC_percent = ((g_count + c_count) / total) * 100
     AT_percent = ((a_count + t_count) / total) * 100
 
-    result_label.config(text=f"""
-              
+    result_label.configure(text=f"""
 Length: {total}
 
 Nucleoid #
@@ -44,12 +67,15 @@ GC%: {GC_percent:.2f}%
 AT%: {AT_percent:.2f}%
 """)
 
-def fill_random():
-    seq = random_dna(30) 
-    entry.delete(0, tk.END)
-    entry.insert(0, seq)
+dna_img = Image.open("images/dna.png")
 
-tk.Button(root, text="Count DNA", command=count_dna, font=("Calligraffitti", 10)).pack(pady=10)
-tk.Button(root, text="Randomize", command=fill_random, font=("Calligraffitti", 10)).pack(pady=10)
+ctk.CTkButton(root, 
+              text="Count DNA", 
+              command=count_dna, 
+              text_color="white", 
+              font=("Calligraffitti", 14), 
+              corner_radius=5,
+              hover_color="#6969E1",
+              image=ctk.CTkImage(dark_image=dna_img, light_image=dna_img)).pack(pady=10)
 
-root.mainloop()
+root.mainloop()  
